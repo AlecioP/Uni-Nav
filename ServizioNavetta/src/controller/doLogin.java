331715375c3a24e668e0cf41309272d78d816a85
Aldo <aldo.criscuolo@yahoo.it>
2019-01-24 15:39:47 +0100

    Test Commit Aldo

---
+++ ServizioNavetta/src/controller/doLogin.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class doLogin
+ */
+@WebServlet(description = "dopo Login home", urlPatterns = { "/doLogin" })
+public class doLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public doLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = (String) request.getAttribute("id");
+		String pass = (String) request.getAttribute("pass");
+		System.out.println(username +" "+ pass);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
